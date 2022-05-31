f6cb2af1509ab120c49fbb0110eeb3d823236f7d
[AleksandrKuznecovGit] <[kuz888ka147@gmail.com]>
2022-05-31 15:06:55 +0700

    первая фиксация

---
+++ in.py
@@ -0,0 +1,13 @@
+# shape_len = float(input("Введите длинну прямоугольника: "))
+# shape_width = float(input("Введите ширину прямоугольника: "))
+# shape_area = shape_len * shape_width
+#
+# print(f"Площадь прямоугольника равна: {shape_area}")
+
+
+your_age = int(input("Сколько вам лет? - "))
+
+if your_age < 18:
+    print(("Подрости сынок"))
+else:
+    print("Добро пожаловать на наш ХАБ :))")
